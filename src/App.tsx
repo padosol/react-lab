a73a227bbecb40e9a906e00c73ea146ac2a7bd4c
hsh <hsh@argonet.co.kr>
2023-07-01 22:03:03 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Router from '../src/router/router';
 
-import Header from './component/layout/Header';
-import Content from './component/layout/Content';
-import Footer from './component/layout/Footer';
+
 
 
 function App() {
 
   return (
     <div className="App ">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Header/> }></Route>  
-          <Route path="/content" element= { <Content /> }></Route>
-          <Route path="/footer" element={ <Footer/> }></Route>
-        </Routes>     
-      </BrowserRouter>
+      <Router />
     </div>
   );
 }
---
+++ src/router/Router.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Header from '../component/layout/Header';
+import Content from '../component/layout/Content';
+import Footer from '../component/layout/Footer';
+
+
+function Router() {
+    return  (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={ <Header/> }></Route>  
+            <Route path="/content" element= { <Content /> }></Route>
+            <Route path="/footer" element={ <Footer/> }></Route>
+        </Routes>     
+    </BrowserRouter>
+
+    )     
+}
+
+export default Router;
