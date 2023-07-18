f8bbe60b5cf089f93508dc1ae8197d7fc0bf800e
hsh <hsh@argonet.co.kr>
2023-07-18 23:08:01 +0900

    typcript적용

--- src/view/test.tsx
+++ src/view/test.tsx
@@ -1,4 +1,23 @@
-const Test =() =>{
+import { useState } from "react";
+
+interface Itest {
+    data: Number,
+    data2: String
+}
+
+const Test = () =>{
+
+    const [data, setData] = useState<Itest>({
+        data: 1,
+        data2: 'test'
+    });
+
+    console.log(data)
+    setData({
+        data: 2,
+        data2: 'abc'
+    });
+
     return (
         <div>test</div>
     )
